19a8fc3aeef7ce8faf62849816ab34829c49799b
Vinicius <vsouto321@gmail.com>
2023-09-30 16:45:24 -0300

    Game seeds

--- src/main/java/com/vinicius/dslist/entities/Game.java
+++ src/main/java/com/vinicius/dslist/entities/Game.java
@@ -17,13 +17,19 @@ public class Game {
 	private Long id;
 	
 	private String title;
+	
 	@Column(name = "game_year")
 	private Integer year;
+	
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
