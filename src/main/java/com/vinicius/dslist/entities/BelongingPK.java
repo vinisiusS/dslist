8cc1d732ef4c9a46030c918e59c14b353facea9a
Vinicius <vsouto321@gmail.com>
2024-01-17 06:08:25 -0300

    Database seed update

--- src/main/java/com/vinicius/dslist/entities/BelongingPK.java
+++ src/main/java/com/vinicius/dslist/entities/BelongingPK.java
@@ -12,7 +12,7 @@ public class BelongingPK {
 	@JoinColumn(name = "game_id")
 	private Game game;
 	@ManyToOne
-	@JoinColumn(name = "gameList_id")
+	@JoinColumn(name = "list_id")
 	private GameList gameList;
 	
 	public BelongingPK() {
