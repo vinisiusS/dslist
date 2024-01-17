2f6daed3a00aaa2b75d85349dc7d76880e15cd7a
Vinicius <vsouto321@gmail.com>
2024-01-17 05:55:59 -0300

    Domain model

--- src/main/java/com/vinicius/dslist/entities/Game.java
+++ src/main/java/com/vinicius/dslist/entities/Game.java
@@ -9,7 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
+@Entity 
 @Table(name = "tb_game")
 public class Game {
 	@Id
