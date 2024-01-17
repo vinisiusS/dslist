f61336ff6a01c5ae8294affbc84ca7efb3126759
Vinicius <vsouto321@gmail.com>
2024-01-17 06:54:28 -0300

    GET lists

---
+++ src/main/java/com/vinicius/dslist/services/GameListService.java
@@ -0,0 +1,23 @@
+package com.vinicius.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vinicius.dslist.dto.GameListDTO;
+import com.vinicius.dslist.entities.GameList;
+import com.vinicius.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	 public List<GameListDTO> findAll(){
+		 List<GameList> result = gameListRepository.findAll();
+		 return result.stream().map(x -> new GameListDTO(x)).toList();
+	 }
+}
